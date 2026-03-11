bba21aeb86fd76011dc851c869ce66ff23306097
“MendoRsh“ <heisenberbad2@gmail.com>
2026-03-11 17:36:49 +0100

    fix Hero api (waitlist)

--- apps/landing/components/Hero.tsx
+++ apps/landing/components/Hero.tsx
@@ -1,4 +1,6 @@
 export const dynamic = "force-dynamic"; // No cache, siempre fresco // SE PUEDE MEJORAR CON ISR SI ES NECESARIO // PERO POR AHORA ASÍ ESTÁ BIEN PARA VER LOS CAMBIOS EN TIEMPO REAL EN LA LISTA DE ESPERA
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 import { getWaitlistCount } from "@/lib/waitlist";
 import HeroClient from "./HeroClient";
